be17a3ac2c34677f2c86ef044ed26e5d6e091ddb
synthmax <max@koblo.com>
2011-08-10 10:03:45 +0200

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # RVM bootstrap
 $:.unshift(File.expand_path("~/.rvm/lib"))
 require 'rvm/capistrano'
-set :rvm_ruby_string, '1.9.2-p180'
+set :rvm_ruby_string, '1.9.2-p136'
 #set :rvm_type, :user
 set :rvm_bin_path, "/usr/local/rvm/bin"
 
